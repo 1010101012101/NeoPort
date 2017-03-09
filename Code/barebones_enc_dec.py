929dbe4d02d5ea569a53b489ba9b790ad575d2e3
Varun Gangal <vgtomahawk@Varuns-MacBook-Pro.local>
2017-03-09 17:38:22 -0500

    Added initialization, beam decoding. Beam decoding doesn't work well at all for higher beam sizes.

--- Code/barebones_enc_dec.py
+++ Code/barebones_enc_dec.py
@@ -9,6 +9,7 @@ import numpy as np
 import datetime
 import nltk
 import copy
+import loadEmbeddingsFile
 
 def attend(encoder_outputs,state_factor_matrix):
     miniBatchLength=state_factor_matrix.npvalue().shape[1]
@@ -125,12 +126,14 @@ class Config:
     sharing=False
     GRU=False
 
-    def __init__(self,READ_OPTION="NORMAL",downstream=True,sharing=False,GRU=False,preTrain=False):
+    def __init__(self,READ_OPTION="NORMAL",downstream=True,sharing=False,GRU=False,preTrain=False,initFromFile=False,initFileName=None):
         self.READ_OPTION=READ_OPTION
         self.downstream=downstream
         self.sharing=sharing
         self.GRU=GRU
         self.preTrain=preTrain
+        self.initFromFile=initFromFile
+        self.initFileName=initFileName
 
 class HyperParams:
     EMB_SIZE=None
@@ -149,7 +152,127 @@ class HyperParams:
         self.STOP=STOP
         self.SEPARATOR=SEPARATOR
 
+def equalSequence(x,y):
+    if len(x)!=len(y):
+        return False
+
+    for i,alpha in enumerate(x):
+        if y[i]!=alpha:
+            return False
+
+    return True
+
 class Model:
+
+    def beamDecode(self,sentence_de,k=1):
+        model=self.model
+        encoder=self.encoder
+        revcoder=self.revcoder
+        decoder=self.decoder
+        encoder_params=self.encoder_params
+        decoder_params=self.decoder_params
+        downstream=self.config.downstream
+        GRU=self.config.GRU
+        hyperParams=self.hyperParams
+
+        part1=sentence_de[:sentence_de.index(hyperParams.SEPARATOR)]+[hyperParams.STOP,]
+        part2=sentence_de[sentence_de.index(hyperParams.SEPARATOR)+1:]
+        part1Length=len(part1)
+        part2Length=len(part2)
+
+        dy.renew_cg() 
+        encoder_lookup=encoder_params["lookup"]
+        decoder_lookup=decoder_params["lookup"]
+        R=dy.parameter(decoder_params["R"])
+        bias=dy.parameter(decoder_params["bias"])
+
+        sentence_de_forward=sentence_de
+        sentence_de_reverse=sentence_de[::-1]
+
+        s=encoder.initial_state()
+        inputs=[dy.lookup(encoder_lookup,de) for de in sentence_de_forward]
+        states=s.add_inputs(inputs)
+        encoder_outputs=[s.output() for s in states]
+
+        s_reverse=revcoder.initial_state()
+        inputs=[dy.lookup(encoder_lookup,de) for de in sentence_de_reverse]
+        states_reverse=s_reverse.add_inputs(inputs)
+        revcoder_outputs=[s.output() for s in states_reverse]
+
+        final_coding_output=encoder_outputs[-1]+revcoder_outputs[-1]
+        final_state=states[-1].s()
+        final_state_reverse=states_reverse[-1].s()
+
+        if GRU:
+            final_coding_state=final_state_reverse+final_state
+        else:
+            final_coding_state=((final_state_reverse[0]+final_state[0]),(final_state_reverse[1]+final_state[1]))
+        final_combined_outputs=[revcoder_output+encoder_output for revcoder_output,encoder_output in zip(revcoder_outputs[::-1],encoder_outputs)]
+
+        s_init=decoder.initial_state().set_s(final_state_reverse)
+        o_init=s_init.output() 
+        alpha_init=dy.softmax(dy.concatenate([dy.dot_product(o_init,final_combined_output) for final_combined_output in final_combined_outputs]))
+        c_init=attend_vector(final_combined_outputs,alpha_init)
+        
+        s_0=s_init
+        o_0=o_init
+        alpha_0=alpha_init
+        c_0=c_init
+       
+        finishedSequences=[]
+        currentSequences=[(s_0,c_0,o_0,[],0.0),]
+
+        #print "Beam Search Start"
+        while len(finishedSequences)<k:
+            candidates=[]
+            for currentSequence in currentSequences:
+                scores=None
+                if downstream:
+                    scores=dy.affine_transform([bias,R,dy.concatenate([currentSequence[2],currentSequence[1]])])
+                else:
+                    scores=dy.affine_transform([bias,R,currentSequence[2]])
+                topkTokens=topk(scores.npvalue(),k)
+                for topkToken in topkTokens:
+                    loss=(dy.pickneglogsoftmax(scores,topkToken)).value()
+                    candidate_i_t=dy.concatenate([dy.lookup(decoder_lookup,topkToken),currentSequence[1]])
+                    candidate_s_t=currentSequence[0].add_input(candidate_i_t)
+                    candidate_o_t=candidate_s_t.output()
+                    candidate_alpha_t=dy.softmax(dy.concatenate([dy.dot_product(candidate_o_t,final_combined_output) for final_combined_output in final_combined_outputs]))
+                    candidate_c_t=attend_vector(final_combined_outputs,candidate_alpha_t)
+                    candidate_loss=currentSequence[4]+loss
+                    candidate_sequence=copy.deepcopy(currentSequence[3])
+                    candidate_sequence.append(topkToken)
+                    candidate=(candidate_s_t,candidate_c_t,candidate_o_t,candidate_sequence,candidate_loss)
+                    if topkToken==hyperParams.STOP or len(candidate_sequence)>len(sentence_de)+20:
+                        if len(candidate_sequence)>2:
+                            finishedSequences.append(candidate)
+                    else:
+                        candidates.append(candidate)
+            #Sort candidates by loss, lesser loss is better
+            candidates.sort(key = lambda x: x[4])
+            currentSequences=candidates[:k]
+
+        #print "Beam Search End"
+
+        prunedFinishedSequences=[]
+        
+        #Remove output sequences identical to parents. If that leaves nothing, fall back onto the original
+        for finishedSequence in finishedSequences:
+            if equalSequence(finishedSequence[3],part1) or equalSequence(finishedSequence[3],part2):
+                continue
+            else:
+                prunedFinishedSequences.append(finishedSequence)
+
+        if len(prunedFinishedSequences)!=0:
+            finishedSequences=prunedFinishedSequences
+
+        finishedSequences.sort(key = lambda x:x[4])
+        sentence_en=finishedSequences[0][3]      
+        loss_en=finishedSequences[0][4]
+
+        return loss_en,sentence_en
+
+
     def greedyDecode(self,sentence_de):
         model=self.model
         encoder=self.encoder
@@ -365,11 +488,21 @@ class Model:
         total_loss=dy.esum(losses)
         return total_loss,total_words
 
+    def save_model(self):
+        print "Saving Model"
+        self.model.save(self.modelFile,[self.encoder,self.revcoder,self.decoder,self.encoder_params["lookup"],self.decoder_params["lookup"],self.decoder_params["R"],self.decoder_params["bias"]])
+        print "Model Saved"
 
+    def load_model(self):
+        print "Loading Model"
+        (self.encoder,self.revcoder,self.decoder,self.encoder_params["lookup"],self.decoder_params["lookup"],self.decoder_params["R"],self.decoder_params["bias"])=self.model.load(self.modelFile)
+        print "Model Loaded"
 
-    def __init__(self,config,hyperParams):
+    def __init__(self,config,hyperParams,modelFile="Buffer/model"):
         self.config=config
         self.hyperParams=hyperParams
+        self.modelFile=modelFile
+        self.bestPerplexity=float("inf")
 
         if self.config.READ_OPTION=="NORMAL":
             train_sentences_de,train_sentences_en,valid_sentences_de,valid_sentences_en,test_sentences_de,test_sentences_en,wids=readData.getData(trainingPoints=700,validPoints=400)
@@ -417,14 +550,24 @@ class Model:
             self.revcoder=dy.LSTMBuilder(hyperParams.LAYER_DEPTH,hyperParams.EMB_SIZE,hyperParams.HIDDEN_SIZE,model)
             self.decoder=dy.LSTMBuilder(hyperParams.LAYER_DEPTH,hyperParams.EMB_SIZE+hyperParams.HIDDEN_SIZE,hyperParams.HIDDEN_SIZE,model)
 
+        if self.config.initFromFile:
+            preEmbeddings=loadEmbeddingsFile.loadEmbeddings(wids,self.config.initFileName,hyperParams.EMB_SIZE)
+            
+    
         self.encoder_params={}
         self.encoder_params["lookup"]=model.add_lookup_parameters((self.VOCAB_SIZE_DE,hyperParams.EMB_SIZE))
+        
+        if self.config.initFromFile:
+            self.encoder_params["lookup"].init_from_array(preEmbeddings)
 
         self.decoder_params={}
         if config.sharing:
             self.decoder_params["lookup"]=self.encoder_params["lookup"]
         else:
             self.decoder_params["lookup"]=model.add_lookup_parameters((self.VOCAB_SIZE_EN,hyperParams.EMB_SIZE))
+
+        if self.config.initFromFile:
+            self.decoder_params["lookup"].init_from_array(preEmbeddings)
 
         if config.downstream:
             self.decoder_params["R"]=model.add_parameters((self.VOCAB_SIZE_EN,2*hyperParams.HIDDEN_SIZE))
@@ -455,7 +598,7 @@ class Model:
             trainer.update_epoch(1.0)
 
 
-    def train(self):
+    def train(self,interEpochPrinting=False):
         if self.config.preTrain:
             print "Pretraining"
             self.pretrain()
@@ -472,25 +615,48 @@ class Model:
                 loss.value()
                 loss.backward()
                 trainer.update()
-                if totalSentences%100==0:
-                    #random.shuffle(valid_sentences)
-                    perplexity=0.0
-                    totalLoss=0.0
-                    totalWords=0.0
-                    for valid_sentence in self.valid_sentences:
-                        valid_sentence_de=valid_sentence[0]
-                        valid_sentence_en=valid_sentence[1]
-                        validLoss,words=self.do_one_example(valid_sentence_de,valid_sentence_en)
-                        totalLoss+=float(validLoss.value())
-                        totalWords+=words
-                    print totalLoss
-                    print totalWords
-                    perplexity=math.exp(totalLoss/totalWords)
-                    print "Validation perplexity after epoch:",epochId,"sentenceId:",sentenceId,"Perplexity:",perplexity,"Time:",datetime.datetime.now()             
+                if interEpochPrinting:
+                    if totalSentences%100==0:
+                        #random.shuffle(valid_sentences)
+                        perplexity=0.0
+                        totalLoss=0.0
+                        totalWords=0.0
+                        for valid_sentence in self.valid_sentences:
+                            valid_sentence_de=valid_sentence[0]
+                            valid_sentence_en=valid_sentence[1]
+                            validLoss,words=self.do_one_example(valid_sentence_de,valid_sentence_en)
+                            totalLoss+=float(validLoss.value())
+                            totalWords+=words
+                        print totalLoss
+                        print totalWords
+                        perplexity=math.exp(totalLoss/totalWords)
+                        print "Validation perplexity after epoch:",epochId,"sentenceId:",sentenceId,"Perplexity:",perplexity,"Time:",datetime.datetime.now()             
             trainer.update_epoch(1.0)
+            perplexity=0.0
+            totalLoss=0.0
+            totalWords=0.0
+            for valid_sentence in self.valid_sentences:
+                valid_sentence_de=valid_sentence[0]
+                valid_sentence_en=valid_sentence[1]
+                validLoss,words=self.do_one_example(valid_sentence_de,valid_sentence_en)
+                totalLoss+=float(validLoss.value())
+                totalWords+=words
+            perplexity=math.exp(totalLoss/totalWords)
+            if epochId>=5 and perplexity<self.bestPerplexity:
+                self.save_model()
+                self.bestPerplexity=perplexity
+                print "Best Perplexity:",self.bestPerplexity
 
+        #self.save_model()
+        #self.load_model()
 
-    def testOut(self,valid_sentences,verbose=True,originalFileName="originalWords.txt",outputFileName="outputWords.txt"):
+    def stripStops(self,sentence):
+        if sentence[-1]==self.hyperParams.STOP:
+            return sentence[:-1]
+        else:
+            return sentence
+
+    def testOut(self,valid_sentences,verbose=True,originalFileName="originalWords.txt",outputFileName="outputWords.txt",decodeMethod="greedy",beam_decode_k=1):
         reverse_wids=self.reverse_wids
 
         originalWordFile=open(originalFileName,"w")
@@ -503,7 +669,13 @@ class Model:
         for validSentenceId,validSentence in enumerate(valid_sentences):
             valid_sentence_de=validSentence[0]
             valid_sentence_en=validSentence[1]
-            validLoss,valid_sentence_en_hat=self.greedyDecode(valid_sentence_de)
+            if decodeMethod=="greedy":
+                validLoss,valid_sentence_en_hat=self.greedyDecode(valid_sentence_de)
+            elif decodeMethod=="beam":
+                validLoss,valid_sentence_en_hat=self.beamDecode(valid_sentence_de,k=beam_decode_k)
+            
+            #valid_sentence_en_stripped=self.stripStops(valid_sentence_en)
+            #valid_sentence_en_hat_stripped=self.stripStops(valid_sentence_en_hat)
 
             originalWord="".join([reverse_wids[c] for c in valid_sentence_en[:-1]])
             outputWord="".join([reverse_wids[c] for c in valid_sentence_en_hat[:-1]])
@@ -534,17 +706,42 @@ class Model:
 
 if __name__=="__main__":
     READ_OPTION="NORMAL"
-    preTrain=True
+    preTrain=False
     downstream=True
     sharing=False
     GRU=False
+    initFromFile=True
+    initFileName="../Pretrained/output_embeddings_134iter_lowestValLoss.txt"
 
-    config=Config(READ_OPTION=READ_OPTION,downstream=downstream,sharing=sharing,GRU=GRU,preTrain=preTrain)
-    hyperParams=HyperParams()
+    config=Config(READ_OPTION=READ_OPTION,downstream=downstream,sharing=sharing,GRU=GRU,preTrain=preTrain,initFromFile=initFromFile,initFileName=initFileName)
+    hyperParams=HyperParams(NUM_EPOCHS=10)
 
     predictor=Model(config,hyperParams)
     predictor.train()
 
-    predictor.testOut(predictor.valid_sentences,verbose=True,originalFileName="originalWords.txt",outputFileName="outputWords.txt")
+    print "Greedy Decode"
+    print "Validation Performance"
+    predictor.testOut(predictor.valid_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt")
+    print "Test Performance"
     predictor.testOut(predictor.test_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt")
+
+    """
+    print "Beam Decode"
+    predictor.testOut(predictor.valid_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt",decodeMethod="beam",beam_decode_k=5)
+    #predictor.testOut(predictor.test_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt",decodeMethod="beam",beam_decode_k=2)
+    """
+
+    predictor.load_model()
+
+    print "Greedy Decode"
+    print "Validation Performance"
+    predictor.testOut(predictor.valid_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt")
+    print "Test Performance"
+    predictor.testOut(predictor.test_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt")
+
+    """
+    print "Beam Decode"
+    predictor.testOut(predictor.valid_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt",decodeMethod="beam",beam_decode_k=5)
+    #predictor.testOut(predictor.test_sentences,verbose=False,originalFileName="originalWords.txt",outputFileName="outputWords.txt",decodeMethod="beam",beam_decode_k=2)
+    """
 
