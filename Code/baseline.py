d00995df3e0d7cfe7aa8c29450a04d2b557405f9
Harsh Jhamtani <harshjhamtani@gmail.com>
2017-03-11 15:43:29 -0500

    Updates to baseline

--- Code/baseline.py
+++ Code/baseline.py
@@ -40,16 +40,34 @@ class BaselineModel:
 		data = [ ''.join(self.reverse(w[:-1])) for w in data ]
 		return data
 
+	def getReverseTuplesLabels(self, data, labels):
+		return self.reverseAllTuples(data), self.reverseAll(labels)
+
 	def evaluateUsingLM(self, lm_model, data):
 		trainInputs,trainOutputs,validInputs,validOutputs,testInputs,testOutputs,wids = data
 		revers_dict = {i:ch for ch,i in wids.items()}
 		self.revers_dict=revers_dict
-		word_train = self.reverseAllTuples( trainInputs )
+		
+		word_train, label_train = self.getReverseTuplesLabels( trainInputs, trainOutputs )
 		print word_train[0]
 		print word_train[1]
-		label_train = self.reverseAll(trainOutputs)
 		print label_train[0]
 		print label_train[1]
+		word_val, label_val = self.getReverseTuplesLabels( validInputs, validOutputs )
+		word_test, label_test = self.getReverseTuplesLabels( testInputs, testOutputs )
+
+		for i,w1w2 in enumerate(word_test):
+			w1,w2=w1w2
+			candidates = utilities.generateCandidates(w1,w2)
+			candidates=[c for c in candidates]
+			all_scores = map(lm_model.getSequenceScore, candidates)
+			best_score, best_score_idx = np.max(all_scores), np.argmax(all_scores)
+			print w1," ",w2
+			print best_score," ",best_score_idx," ", candidates[best_score_idx]
+			print ""
+			if i>5:
+				break
+
 
 	def evaluate(self,params):
 		data = self.data
