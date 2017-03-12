921412b7737fa0f4076a3bd6194ecaf1391284d6
Varun Gangal <vgtomahawk@Varuns-MBP.hsd1.pa.comcast.net>
2017-03-11 23:01:10 -0500

    Updated model

--- Code/utilities.py
+++ Code/utilities.py
@@ -7,9 +7,9 @@ def generateCandidates(w1, w2):
     for i in range(1,n+1):
         for j in range(m):
             all_candidates.append(w1[0:i]+w2[j:])
-    print "len(all_candidates)= ",len(all_candidates)
+    #print "len(all_candidates)= ",len(all_candidates)
     all_candidates=set(all_candidates)
-    print "len(all_candidates) after deduplication= ",len(all_candidates)
+    #print "len(all_candidates) after deduplication= ",len(all_candidates)
     return all_candidates
 
 if __name__=="__main__":
